ff1d2c8116a878a326e227d474a2e14a87471076
anngilgun <annfox41@gmail.com>
2022-09-19 18:41:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -53,6 +53,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function search(event) {
