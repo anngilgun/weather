cb12ea5953a5c44b2126dc63168c386fb9987a0d
anngilgun <annfox41@gmail.com>
2022-09-19 18:25:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,6 +51,8 @@ function displayForecast(response) {
 }
 function getForecast(coordinates) {
   console.log(coordinates);
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 }
 
 function search(event) {
