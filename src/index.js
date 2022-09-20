7703f71c960ee469b65806fe382a264363cf242d
anngilgun <annfox41@gmail.com>
2022-09-20 18:27:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -125,28 +125,6 @@ forms.addEventListener("submit", cityPut);
 
 //currentcity
 
-function showWeather(response) {
-  let cities = document.querySelector("#city");
-  cities.innerHTML = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  let h1 = document.querySelector("h1");
-  let weaather = `${temperature}°C `;
-  h1.innerHTML = weaather;
-  let description = document.querySelector("#weather-description");
-  description.innerHTML = response.data.weather[0].description;
-  document.querySelector("#humidity").innerHTML = `Humidity: ${Math.round(
-    response.data.main.humidity
-  )}%`;
-  document.querySelector("#wind").innerHTML = `Wind: ${Math.round(
-    response.data.wind.speed
-  )}km/h`;
-  let iconElement = document.querySelector("#em1");
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
-}
 
 function displayPosition(position) {
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
@@ -154,6 +132,9 @@ function displayPosition(position) {
   let lon = position.coords.longitude;
 
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(`${url}`).then(showWeather);
+  axios.get(`${url}`).then(showTemperature);
 }
+function getCurrentLocation(event) {
+  event.preventDefault();
 navigator.geolocation.getCurrentPosition(displayPosition);
+
